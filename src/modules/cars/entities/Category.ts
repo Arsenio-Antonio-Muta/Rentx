e3cd5a50e1f31b385bb437e739f1328e29c28c4b
Arsenio Antonio Muta <arsenioantoniomuta@gmail.com>
2022-10-06 20:46:46 +0100

    insercao do docker no projecto e criacao de uma base de dados real

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string | undefined;
+
+  @Column()
   description: string | undefined;
+
+  @CreateDateColumn()
   created_at: Date | undefined;
 
   constructor() {
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -9,8 +9,10 @@ class CreateCategoryUseCase {
   // eslint-disable-next-line prettier/prettier
   constructor(private categoryRepository: ICategoriesRepository) { }
 
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadyExists = this.categoryRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoryRepository.findByName(
+      name
+    );
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists");
