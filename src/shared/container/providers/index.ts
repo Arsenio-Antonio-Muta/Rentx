f25600e3249dacc4e4ab68fdc81590a823de07cd
Arsenio Antonio Muta <arsenioantoniomuta@gmail.com>
2022-10-27 16:53:51 +0100

    Implementado feature de recuperação de password

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,9 +4,16 @@ import "@shared/container/providers";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementetion/DayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementation/EtherealMailProvider";
 
 
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherealMailProvider",
+  new EtherealMailProvider()
 );
